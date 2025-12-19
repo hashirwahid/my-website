c180ab12212dd5f68df461bb4c7bafed76f11477
hashirwahid <hashirwahid8@gmail.com>
2025-12-19 23:33:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function showDetail(id) {
-  const all = document.querySelectorAll('.detail');
-  all.forEach(d => d.style.display = 'none');
+    const all = document.querySelectorAll('.detail-content');
+    all.forEach(d => d.style.display = 'none');
 
-  const selected = document.getElementById(id);
-  selected.style.display = 'block';
-  selected.scrollIntoView({ behavior: 'smooth' });
+    const selected = document.getElementById(id);
+    selected.style.display = 'block';
+    selected.scrollIntoView({ behavior: 'smooth' });
 }
